9c0ea05cfee10d4a8f5499772e73bc43a8c8ebe3
PlusHistoriae <85767310+PlusHistoriae@users.noreply.github.com>
2022-10-16 23:25:18 +0000

    Initial commit

---
+++ isgModule.py
@@ -0,0 +1,115 @@
+def interfacePresenter():
+	print("IDIOTIC STORY GENERATOR Interface:")
+	print("  Code by PlusHistoriae and ideation help from Fr05T.")
+	print("  Story formats from 'storystar.com.'")
+	print()
+	print()
+
+def storyOne():
+	pn1 = input("One plural noun (eg. kangaroos): ")
+	pn2 = input("A second plural noun (eg. kangaroos): ")
+	pn3 = input("A third plural noun (eg. kangaroos): ")
+	pn4 = input("A fourth plural noun (eg. kangaroos): ")
+	pn5 = input("A fifth plural noun (eg. kangaroos): ")
+	pn6 = input("A sixth plural noun (eg. kangaroos): ")
+	pn7 = input("A seventh plural noun (eg. kangaroos): ")
+	print()
+	
+	n1 = input("One singular noun (eg. pencil): ")
+	n2 = input("A second singular noun (eg. pencil): ")
+	n3 = input("A third noun (eg. pencil): ")
+	n4 = input("A fourth noun (eg. pencil): ")
+	n5 = input("A fifth noun (eg. pencil): ")
+	n6 = input("A sixth noun (eg. pencil): ")
+	n7 = input("A seventh noun (eg. pencil): ")
+	n8 = input("A eigth noun (eg. pencil): ")
+	print()
+
+	ppn1_tn1 = input("1 proper noun or regular noun following the word “the” (eg. the dog): ")
+	print()
+
+	pl1 = input("One place or location (eg. school): ")
+	pl2 = input("Another place or location (eg. school): ")
+	print()
+
+	ad1 = input("One adjective (eg. sticky): ")
+	ad2 = input("A second adjective (eg. sticky): ")
+	ad3 = input("A third adjective (eg. sticky): ")
+	ad4 = input("A fourth adjective (eg. sticky): ")
+	ad5 = input("A fifth adjective (eg. sticky): ")
+	ad6 = input("A sixth adjective (eg. sticky): ")
+	ad7 = input("A seventh adjective (eg. sticky): ")
+	ad8 = input("A eigth adjective (eg. sticky): ")
+	ad9 = input("A ninth adjective (eg. sticky): ")
+	print()
+
+	ea1 = input("One emotional adjective (eg. somber): ")
+	print()
+
+	v1 = input("One present-tense verb (eg. run, or see): ")
+	v2 = input("A second present-tense verb (eg. run, or see): ")
+	v3 = input("A third present-tense verb (eg. run, or see): ")
+	v4 = input("A fourth present-tense verb (eg. run, or see): ")
+	v5 = input("A fifth present-tense verb (eg. run, or see): ")
+	v6 = input("A sixth present-tense verb (eg. run, or see): ")
+	v7 = input("A seventh present-tense verb (eg. run, or see): ")
+	print()
+
+	ptv1 = input("One past-tense verb (eg. ran): ")
+	ptv2 = input("A second past-tense verb (eg. ran): ")
+	ptv3 = input("A third past-tense verb (eg. ran): ")
+	ptv4 = input("A fourth past-tense verb (eg. ran): ")
+	ptv5 = input("A fifth past-tense verb (eg. ran): ")
+	ptv6 = input("A sixth past-tense verb (eg. ran): ")
+	ptv7 = input("A seventh past-tense verb (eg. ran): ")
+	ptv8 = input("A eigth past-tense verb (eg. ran): ")
+	ptv9 = input("A ninth past-tense verb (eg. ran): ")
+	print()
+
+	iv1 = input("One verb that ends in the suffix 'ing': ")
+	iv2 = input("Another verb that ends in the suffix 'ing': ")
+	print()
+	
+	print (pn1 + "... " + pn1 + " are what drove us apart. Our " + n1 + " was " + ad1 + ", our " + n1 + " was " + ad2 + ", our " + n1 + " was " + ad3 + ". I blame " + ppn1_tn1 + ". You were " + ad4 + ". You were always there, always made me feel like you were at least. You probably did not care but you " + ptv1 + " me. When I was at my " + pl1 + " and nobody " + ptv2 + " you always did. It made me feel " + ad5 + " when you switched from " + iv1 + " me to only talking to me in a big crowd. It made me " + ad5 + " when you finally knew my name. It was my fault, yet I blame the " + n2 + ". I miss you. I miss having someone I can call my " + n3 + ". I want you not " + iv2 + " me to not bother me but it does. I want to " + v1 + " you but I can’t. We were so close and I know the old you is inside the " + n4 + ". It was this summer the day my " + pn2 + " told me to tell you that I " + ptv3 + " you. I did, I gave in. It was " + ad6 + " because you were obsessed with " + pn3 + ". It started as a little " + n5 + " that I didn’t care about, a " + n5 + " I wasn’t going to " + v2 + ".  As soon as I did you " + ptv4 + ". You " + ptv5 + " to protect my feelings like you always do, but this time I could " + v3 + " right through you. I " + ptv6 + " like nothing happened. I " + ptv7 + " you, I " + ptv8 + " you, I " + ptv9 + " you, nothing… It was like I didn’t mean anything to you. Now in the hallways it’s like I am a " + n6 + ". Every time I " + v4 + " you my " + n7 + " breaks more and more. I don’t want you to " + v1 + " me, I want you to say something. Tell me you don’t want to be my " + n3 + ", tell me to stop " + iv2 + ", yet you never do. I am " + ad7+ ". I see you talking to my old " + pn4 + " that we always talked about, but not me. Not only this but you never did " + v5 + " me. You felt " + ea1 + " for me. You knew I had no " + pn5 + " and tried to be " + ad8 + ". You were. This time I won’t " + v6 + " like I did for you. You tell people you should never " + v7 + " things but not me. If I didn’t tell you I had " + pn6 + " for you, things would be " + ad9 + ". I have cut myself off from showing " + pn7 + ". I’m the " + n8 + " in the " + pl2 + "."
+)
+def storyTwo():
+	print()
+
+def storyThree():
+	print()
+
+def storyFour():
+	print()
+
+def storyFive():
+	print()
+
+def storySix():
+	print()
+
+def storySeven():
+	print()
+
+def storyEight():
+	print()
+
+def storyNine():
+	print()
+
+def storyTen():
+	print()
+
+def storyEleven():
+	print()
+
+def storyTwelve():
+	print()
+
+def storyThirteen():
+	print()
+
+def storyFourteen():
+	print()
+
+def storyFifteen():
+	print()
---
+++ main.py
@@ -0,0 +1,51 @@
+import random
+import isgModule
+
+isgModule.interfacePresenter()
+
+storyNum = random.randint(1,1)
+
+if storyNum == 1: 
+	isgModule.storyOne()
+
+if storyNum == 2: 
+	isgModule.storyTwo()
+
+if storyNum == 3: 
+	isgModule.storyThree()
+
+if storyNum == 4: 
+	isgModule.storyFour()
+
+if storyNum == 5: 
+	isgModule.storyFive()
+
+if storyNum == 6: 
+	isgModule.storySix()
+
+if storyNum == 7: 
+	isgModule.storySeven()
+
+if storyNum == 8: 
+	isgModule.storyEight()
+
+if storyNum == 9: 
+	isgModule.storyNine()
+
+if storyNum == 10: 
+	isgModule.storyTen()
+
+if storyNum == 11: 
+	isgModule.storyEleven()
+
+if storyNum == 12: 
+	isgModule.storyTwelve()
+
+if storyNum == 13: 
+	isgModule.storyThirteen()
+
+if storyNum == 14: 
+	isgModule.storyFourteen()
+
+if storyNum == 15: 
+	isgModule.storyFifteen()
